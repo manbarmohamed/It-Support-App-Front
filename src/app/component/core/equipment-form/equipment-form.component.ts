b6cb31ccea1ab4d8cb621c00167e35146d7181d8
manbarmohamed <you@example.com>
2024-08-11 14:35:18 +0100

    ajouter le component form pour ajouter et modifier un equipement

--- src/app/component/core/equipment-form/equipment-form.component.ts
+++ src/app/component/core/equipment-form/equipment-form.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EquipementServiceService } from '../../../service/equipement-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { EquipementDto } from '../../../dto/equipement-dto';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-equipment-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './equipment-form.component.html',
   styleUrl: './equipment-form.component.css'
 })
-export class EquipmentFormComponent {
+export class EquipmentFormComponent implements OnInit {
+  equipmentForm: FormGroup;
+  equipmentId: number | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private equipmentService: EquipementServiceService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.equipmentForm = this.fb.group({
+      nome: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.equipmentId = this.route.snapshot.params['id'];
+
+    if (this.equipmentId) {
+      this.loadEquipment(this.equipmentId);
+    }
+  }
+
+  loadEquipment(id: number): void {
+    this.equipmentService.getEquipmentById(id).subscribe((equipment) => {
+      this.equipmentForm.patchValue({
+        nome: equipment.name,
+      });
+    });
+  }
+
+  onSubmit(): void {
+    if (this.equipmentForm.valid) {
+      const equipmentDto: EquipementDto = this.equipmentForm.value;
+      if (this.equipmentId) {
+        this.equipmentService.updateEquipment(this.equipmentId, equipmentDto).subscribe(() => {
+          alert('Équipement mis à jour avec succès!');
+          this.router.navigate(['/equipment']);
+        });
+      } else {
+        this.equipmentService.createEquipment(equipmentDto).subscribe(() => {
+          alert('Équipement créé avec succès!');
+          this.router.navigate(['/equipment']);
+        });
+      }
+    }
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/equipment']);
+  }
 }
